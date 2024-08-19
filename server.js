16d42c6c3532fd2925be477a13b8ca7d88c16fde
yasser-eddouche <eddouchey@gmail.com>
2024-08-19 22:58:10 +0100

    auth methods

--- server.js
+++ server.js
@@ -2,19 +2,32 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
+const bodyParser = require("body-parser");
+const connectDB = require("./server/config/db");
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
+connectDB();
+
 app.use(express.static("public"));
+
+// Middleware to parse JSON and urlencoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // TEMPLATE ENGINE
 app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.use("/", require("./routes/auth"));
+// Import routes
+const mainRoutes = require("./server/routes/main");
+const authRoutes = require("./server/routes/auth");
+
+// Use routes
+app.use(mainRoutes);
+app.use(authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ server/routes/auth.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.get("/user/login", (req, res) => {
+  res.render("auth/login", { title: "Login" });
+});
+
+router.post("/user/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }).lean().exec();
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.render("auth/login", {
+        title: "Login",
+        error: "Invalid email/password",
+      });
+    }
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("auth/login", {
+      title: "Login",
+      error: "Something went wrong",
+    });
+  }
+});
+
+router.get("/user/register", (req, res) => {
+  res.render("auth/register", { title: "register" });
+});
+
+router.post("/user/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const type = "user"; // Set type to 'user' here
+
+  if (!username || !email || !password || !type) {
+    return res.render("auth/register", {
+      title: "register",
+      error: "All fields are required",
+    });
+  }
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user with the hashed password
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      type,
+    });
+    return res.redirect("/user/login");
+  } catch (error) {
+    return res.render("auth/register", {
+      title: "register",
+      error: "Something went wrong",
+    });
+  }
+});
+
+module.exports = router;
