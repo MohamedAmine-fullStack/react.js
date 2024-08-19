aab5523c4347f040a8740c3706100d66ad8517e5
marwaneigou <igourramenmarwane@gmail.com>
2024-08-20 00:44:34 +0100

    some change

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,27 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
+const Product = require('../models/Product'); // Adjust the path as needed
+
 
 
 router.get("/", (req, res) => {
   if (req.session.user) {
-    res.redirect("/dashboard"); // Redirect to dashboard if logged in
+    if (req.session.user.type === 'admin') {
+      res.redirect("/admin/dashboard"); // Redirect to admin dashboard
+    } else if (req.session.user.type === 'user') {
+      res.redirect("/user/dashboard"); // Redirect to user dashboard
+    } else {
+      res.redirect("/user/login"); // Redirect to login if type is not recognized
+    }
   } else {
     res.redirect("/user/login"); // Redirect to login if not logged in
   }
 });
 
-// In server/routes/main.js
-router.get('/dashboard', (req, res) => {
-  if (!req.session.user) {
-    return res.redirect('/user/login'); // Redirect if not authenticated
-  }
-
-  res.render('dashboard', {
-    title: 'Dashboard',
-    user: req.session.user // Pass user data to the view
-  });
-});
 
 
 module.exports = router;
