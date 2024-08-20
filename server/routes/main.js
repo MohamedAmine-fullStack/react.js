670585b685a301c7a60324af7ea2835281eacfba
marwaneigou <igourramenmarwane@gmail.com>
2024-08-20 01:42:12 +0100

    add user form

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
-const Product = require('../models/Product'); // Adjust the path as needed
-
-
 
 router.get("/", (req, res) => {
   if (req.session.user) {
