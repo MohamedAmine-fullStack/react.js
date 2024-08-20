2bf0278a24cc11706d21579ed1bfa4680a8ce424
yasser-eddouche <eddouchey@gmail.com>
2024-08-20 03:17:57 +0100

    merge all

--- server/models/Product.js
+++ server/models/Product.js
@@ -26,33 +26,3 @@ const productSchema = new mongoose.Schema({
 // Create and export the Product model
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
-
-const mongoose = require("mongoose");
-
-// Define the Product schema
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  image: {
-    type: String,
-    trim: true,
-  },
-});
-
-// Create the Product model
-const Product = mongoose.model("Product", productSchema);
-
-// Export the Product model
-module.exports = Product;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
-const productController = require("../controllers/productController");
 
 // Middleware to check authentication
 const isAuthenticated = (req, res, next) => {
